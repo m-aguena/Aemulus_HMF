b8a7b713fdb56bc5987686d9fe3289c9b10cb3ce
Naminoshi <michel.aguena@gmail.com>
2018-11-14 17:42:14 -0200

    fix cosmosis module

--- aemHMF/cosmosis_module.py
+++ aemHMF/cosmosis_module.py
@@ -19,19 +19,42 @@ def setup(options):
     return config
 
 def execute(block, config):
+    '''
+    CAMB parameters:
+        omega_b:  real, baryon density fraction today
+        omega_lambda:  real, dark energy density fraction today
+        omega_k:  real, curvature density fraction today (default 0.0)
+        hubble:  real, hubble parameter H0 (km/s/Mpc)
+        A_s:  real, scalar spectrum primordial amplitude (ignored in background/thermal mode)
+        n_s:  real, scalar spectral index (ignored in background/thermal mode)
+        w:  real, w(z=0) equation of state of dark energy (default -1.0). ignored if use_tabulated_w=T
+        wa:  real, equation of state parameter w(z) = w_0 + w_a z / (1+z)  (default 0.0). ignored if use_tabulated_w=T
+        massless_nu:  real, effective number of massless neutrinos (default 3.046)
+    '''
 
-    OmM = block[names.cosmological_parameters, "omega_m"]
-    cosm = {"om":0.3,
-            "ob":0.05,
-            "ol":1.-0.3,
-            "ok":0.0,
-            "h":0.7,
-            "s8":0.77,
-            "ns":0.96,
-            "w0":-1.0,
-            "wa":0.0,
-            "Neff":3.0}
+    cosm = {"om":0.3, "ob":0.05, "ol":1.-0.3, "ok":0.0, "h":0.7,
+             "s8":0.77, "ns":0.96, "w0":-1.0, "wa":0.0, "Neff":3.0}
 
+    cosm_dic = {
+        # used by emulator
+        "om": "omega_m",
+        "ob": "omega_b",
+        "w0": "w",
+        "ns": "n_s",
+        "h": "hubble",
+        #"Neff": "massless_nu",
+        #"s8": "A_s",# still has to be completed
+        # not used by emulator
+        "ol": "omega_lambda",
+        "ok": "omega_k",
+        "wa": "wa",
+            }
+    for n, p in cosm_dic.items():
+        cosm[n] = block[names.cosmological_parameters, p]
+    cosm['h'] *= 0.01
+
+    print('cosm:',cosm)
+        
     # Just a simple rename for clarity.
     sigma_dir = config.sigma_dir
 
