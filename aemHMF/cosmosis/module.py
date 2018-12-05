ba858b37da87850cb4907f39109b758431b3eb95
Naminoshi <michel.aguena@gmail.com>
2018-12-05 15:48:55 -0200

    make module get s8 from cosmosis block

--- aemHMF/cosmosis/module.py
+++ aemHMF/cosmosis/module.py
@@ -18,8 +18,13 @@ def setup(options):
 
     return config
 
+rho_c = 2.775e11
+log_rho_c_4pi_o3_r8 = np.log10(8**3 * rho_c * 4 * np.pi / 3.)
 def execute(block, config):
     '''
+    Note
+    ----
+
     CAMB parameters:
         omega_b:  real, baryon density fraction today
         omega_lambda:  real, dark energy density fraction today
@@ -30,10 +35,14 @@ def execute(block, config):
         w:  real, w(z=0) equation of state of dark energy (default -1.0). ignored if use_tabulated_w=T
         wa:  real, equation of state parameter w(z) = w_0 + w_a z / (1+z)  (default 0.0). ignored if use_tabulated_w=T
         massless_nu:  real, effective number of massless neutrinos (default 3.046)
+
+    ATENTION!!!
+        Neff is HADRCODED with fixed value of 3.0!
     '''
 
     cosm = {"om":0.3, "ob":0.05, "ol":1.-0.3, "ok":0.0, "h":0.7,
              "s8":0.77, "ns":0.96, "w0":-1.0, "wa":0.0, "Neff":3.0}
+    cosm = {"Neff":3.0}
 
     cosm_dic = {
         # used by emulator
@@ -43,7 +52,6 @@ def execute(block, config):
         "ns": "n_s",
         "h": "hubble",
         #"Neff": "massless_nu",
-        #"s8": "A_s",# still has to be completed
         # not used by emulator
         "ol": "omega_lambda",
         "ok": "omega_k",
@@ -59,8 +67,6 @@ def execute(block, config):
     sigma_dir = config.sigma_dir
 
     # Load sigma(M) from the block
-    #m, z, sig2 = block.get_grid(sigma_dir, "m", "z", "sigma2")
-    #z, m, sig2 = block.get_grid(sigma_dir, "z", "m", "sigma2")
     m = block[sigma_dir, 'm']
     z = block[sigma_dir, 'z']
     sig = np.sqrt(block[sigma_dir, 'sigma2'])
@@ -74,18 +80,14 @@ def execute(block, config):
             z = 1./a - 1.
             return sig_interp(np.log10(M), z)[0][0]
 
-    '''
-    a = 1./(1.+z)
-    aem = tinkerMF()
-    aem.set_cosmology(cosm)
-    mf = np.array([[aem.dndlM(m_, a_) for a_ in a] for m_ in 10**m])
-    '''
+    #m8 = log_rho_c_4pi_o3_r8 + np.log10(cosm['om'])
+    #cosm['s8'] = sig_interp(m8, 0)[0][0]
+    cosm['s8'] = block[names.cosmological_parameters, 'sigma_8']
 
     aem = tinkerMF(sigma_obj=CosmosisSigma())
     aem.set_cosmology(cosm)
     mf = np.array([[aem.dndlM(m_, a_) for a_ in 1./(1.+z)] for m_ in 10**m])
 
-    #block.put_grid("mf", "mf", mf, "z", z_vec, "sigma2", sigma_m.T)
     print mf.shape
     block["mf", "mf" ] = mf
 
